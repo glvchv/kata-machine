31abd3b9325c44f72655b68f08e9a5af617ec437
glvchv <glavchev.vld@gmail.com>
2025-12-09 10:36:11 +0200

    stack and queue

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,9 +1,73 @@
+type Node<T> = {
+  value: T;
+  prev?: Node<T>;
+};
+
+// export default class Stack<T> {
+//   public length: number;
+//   private head?: Node<T>;
+
+//   constructor() {
+//     this.length = 0;
+//     this.head = undefined;
+//   }
+
+//   push(item: T): void {
+//     const node = { value: item, prev: undefined } as Node<T>;
+//     this.length++;
+//     if (!this.head) {
+//       this.head = node as Node<T>;
+//       return;
+//     }
+//     const head = this.head;
+//     this.head = node;
+//     this.head.prev = head;
+//   }
+//   pop(): T | undefined {
+//     if (!this.head) return undefined;
+//     this.length--;
+//     const head = this.head;
+//     this.head = this.head.prev;
+//     return head.value;
+//   }
+//   peek(): T | undefined {
+//     return this.head?.value;
+//   }
+// }
+
+// Prime's implementation
+
 export default class Stack<T> {
   public length: number;
+  private head?: Node<T>;
 
-  constructor() {}
+  constructor() {
+    this.length = 0;
+    this.head = undefined;
+  }
 
-  push(item: T): void {}
-  pop(): T | undefined {}
-  peek(): T | undefined {}
+  push(item: T): void {
+    const node = { value: item } as Node<T>;
+    this.length++;
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+    node.prev = this.head;
+    this.head = node;
+  }
+  pop(): T | undefined {
+    this.length = Math.max(0, this.length - 1);
+    if (this.length === 0) {
+      const head = this.head;
+      this.head = undefined;
+      return head?.value;
+    }
+    const head = this.head as Node<T>;
+    this.head = head.prev;
+    return head.value;
+  }
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }

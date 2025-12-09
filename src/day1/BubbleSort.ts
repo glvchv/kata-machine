801de3fb05024637761bf2dcfbb3f944d8a92a28
glvchv <glavchev.vld@gmail.com>
2025-12-09 10:34:29 +0200

    bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,1 +1,14 @@
-export default function bubble_sort(arr: number[]): void {}
+export default function bubble_sort(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    let swapped = false;
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+}
